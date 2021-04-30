568b4c0250ac9171d3772441c0cd9a0f49590e65
polydome <polydome@outlook.com>
2021-04-30 20:29:04 +0200

    Initialize application

---
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.polydome.popstash.app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.github.polydome.popstash.app.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
