20bc346706c8b0e4ed4362d3f154428247bc2ff7
polydome <polydome@outlook.com>
2021-05-01 08:19:26 +0200

    Create stash list skeleton

---
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/adapter/StashAdapter.kt
@@ -0,0 +1,22 @@
+package com.github.polydome.popstash.app.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.github.polydome.popstash.app.R
+import com.github.polydome.popstash.app.viewholder.ResourceViewHolder
+
+class StashAdapter(private val layoutInflater: LayoutInflater) : RecyclerView.Adapter<ResourceViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResourceViewHolder {
+        val view =  layoutInflater.inflate(R.layout.row_resource, parent, true)
+        return ResourceViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ResourceViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+}
---
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/viewholder/ResourceViewHolder.kt
@@ -0,0 +1,7 @@
+package com.github.polydome.popstash.app.viewholder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ResourceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+}
