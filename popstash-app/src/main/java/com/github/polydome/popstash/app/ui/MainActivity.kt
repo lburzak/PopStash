d85eb5d567fc0c9f8bc983c4506a8d3fc1dbc977
polydome <polydome@outlook.com>
2021-05-01 20:14:42 +0200

    Implement monitoring clipboard for URLs

--- popstash-app/src/main/java/com/github/polydome/popstash/app/ui/MainActivity.kt
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/ui/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.github.polydome.popstash.app.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.polydome.popstash.app.R
 import com.github.polydome.popstash.app.adapter.StashAdapter
+import com.github.polydome.popstash.app.databinding.ActivityMainBinding
 import com.github.polydome.popstash.app.di.BoundViewModel
 import com.github.polydome.popstash.app.viewmodel.StashViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    @Inject lateinit var stashAdapter: StashAdapter
-    @Inject lateinit var linearLayoutManager: LinearLayoutManager
-    @Inject @BoundViewModel lateinit var stashViewModel: StashViewModel
+class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var stashAdapter: StashAdapter
+    @Inject
+    lateinit var linearLayoutManager: LinearLayoutManager
+    @Inject
+    @BoundViewModel
+    lateinit var viewModel: StashViewModel
 
     private val stashRecyclerView: RecyclerView
         get() = findViewById(R.id.stash_recycler_view)
@@ -23,11 +30,23 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+                .apply {
+                    viewModel = this@MainActivity.viewModel
+                    lifecycleOwner = this@MainActivity
+                }
+
         stashRecyclerView.apply {
             adapter = stashAdapter
             layoutManager = linearLayoutManager.apply {
                 orientation = LinearLayoutManager.VERTICAL
             }
         }
+
+        viewModel.isUrlInClipboard.observe(this) {
+            Log.d("StashViewModel", "viewModel.isUrlInClipboard: %s".format(it))
+        }
+
+        viewModel.monitorClipboard()
     }
 }
--- popstash-app/src/main/java/com/github/polydome/popstash/app/viewmodel/StashViewModel.kt
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/viewmodel/StashViewModel.kt
@@ -7,31 +7,24 @@ import androidx.lifecycle.viewModelScope
 import com.github.polydome.popstash.domain.usecase.SaveResource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class StashViewModel @Inject constructor(private val saveResource: SaveResource,
-                     private val clipboard: Clipboard) : ViewModel() {
-
-    init {
-        viewModelScope.launch {
-            clipboardContents.collect {
-                _isUrlInClipboard.postValue(it)
-            }
-        }
-    }
+class StashViewModel @Inject constructor(
+        private val saveResource: SaveResource,
+        private val clipboard: Clipboard,
+) : ViewModel() {
 
     private val clipboardContents = flow {
         while (true) {
             emit(clipboard.getText())
-            delay(5 * 60 * 1000)
+            delay(5 * 1000)
         }
-    }.map { content -> android.util.Patterns.WEB_URL.matcher(content).matches() }
+    }
 
     private val _isUrlInClipboard = MutableLiveData<Boolean>()
     val isUrlInClipboard: LiveData<Boolean> = _isUrlInClipboard
@@ -42,6 +35,16 @@ class StashViewModel @Inject constructor(private val saveResource: SaveResource,
             withContext(Dispatchers.IO) {
                 saveResource.execute(url)
             }
+        }
+    }
+
+    fun monitorClipboard() {
+        viewModelScope.launch {
+            clipboardContents
+                    .map { content -> android.util.Patterns.WEB_URL.matcher(content).matches() }
+                    .collect {
+                        _isUrlInClipboard.postValue(it)
+                    }
         }
     }
 }
