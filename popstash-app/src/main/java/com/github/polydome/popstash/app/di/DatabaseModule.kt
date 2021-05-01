c87e1c22539a651841f1aaa0b205f53ba893f2ea
polydome <polydome@outlook.com>
2021-05-01 19:18:50 +0200

    Setup `:popstash-data` injection

---
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/di/DatabaseModule.kt
@@ -0,0 +1,30 @@
+package com.github.polydome.popstash.app.di
+
+import android.content.Context
+import androidx.room.Room
+import com.github.polydome.popstash.data.AppDatabase
+import com.github.polydome.popstash.data.dao.ResourceDao
+import com.github.polydome.popstash.data.repository.LocalResourceRepository
+import com.github.polydome.popstash.domain.repository.ResourceRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    fun resourceRepository(localResourceRepository: LocalResourceRepository): ResourceRepository =
+            localResourceRepository
+
+    @Provides
+    fun resourceDao(appDatabase: AppDatabase): ResourceDao =
+            appDatabase.resourceDao()
+
+    @Provides
+    fun appDatabase(@ApplicationContext context: Context): AppDatabase =
+            Room.databaseBuilder(context, AppDatabase::class.java, "popstash-db")
+                    .build()
+}
--- popstash-data/src/main/java/com/github/polydome/popstash/data/repository/LocalResourceRepository.kt
+++ popstash-data/src/main/java/com/github/polydome/popstash/data/repository/LocalResourceRepository.kt
@@ -1,18 +1,17 @@
 package com.github.polydome.popstash.data.repository
 
 import com.github.polydome.popstash.data.dao.ResourceDao
-import com.github.polydome.popstash.data.entity.ResourceEntity
 import com.github.polydome.popstash.data.entity.toEntity
 import com.github.polydome.popstash.domain.model.Resource
 import com.github.polydome.popstash.domain.repository.ResourceRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharedFlow
+import javax.inject.Inject
 
-class LocalResourceRepository(private val resourceDao: ResourceDao) : ResourceRepository {
+class LocalResourceRepository @Inject constructor(private val resourceDao: ResourceDao) : ResourceRepository {
     override suspend fun insertOne(resource: Resource) {
         resourceDao.insertOne(resource.toEntity())
     }
 
     override fun watchAllUrls(): Flow<List<String>> =
-        resourceDao.getAllUrls()
+            resourceDao.getAllUrls()
 }
