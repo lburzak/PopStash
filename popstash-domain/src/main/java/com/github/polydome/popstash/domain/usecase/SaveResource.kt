7c367a2ee2cc902970e40a53a243963700c258d9
polydome <polydome@outlook.com>
2021-05-01 19:19:06 +0200

    Setup `:popstash-domain` injection

--- popstash-domain/src/main/java/com/github/polydome/popstash/domain/usecase/SaveResource.kt
+++ popstash-domain/src/main/java/com/github/polydome/popstash/domain/usecase/SaveResource.kt
@@ -2,8 +2,9 @@ package com.github.polydome.popstash.domain.usecase
 
 import com.github.polydome.popstash.domain.model.Resource
 import com.github.polydome.popstash.domain.repository.ResourceRepository
+import javax.inject.Inject
 
-class SaveResource(private val resourceRepository: ResourceRepository) {
+class SaveResource @Inject constructor(private val resourceRepository: ResourceRepository) {
     suspend fun execute(url: String) {
         val resource = Resource(url = url)
 
