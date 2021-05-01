fda54d9547d69542b10d272e93b14da4359d811c
polydome <polydome@outlook.com>
2021-05-01 12:30:44 +0200

    Create model for stash list

--- popstash-app/src/main/java/com/github/polydome/popstash/app/adapter/StashAdapter.kt
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/adapter/StashAdapter.kt
@@ -2,18 +2,18 @@ package com.github.polydome.popstash.app.adapter
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.github.polydome.popstash.app.model.StashListModel
 import com.github.polydome.popstash.app.viewholder.ResourceViewHolder
 import javax.inject.Inject
 
-class StashAdapter @Inject constructor(private val viewHolderFactory: ResourceViewHolder.Factory) : RecyclerView.Adapter<ResourceViewHolder>() {
+class StashAdapter @Inject constructor(private val viewHolderFactory: ResourceViewHolder.Factory,
+                                       private val model: StashListModel) : RecyclerView.Adapter<ResourceViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResourceViewHolder =
             viewHolderFactory.create(parent)
 
     override fun onBindViewHolder(holder: ResourceViewHolder, position: Int) {
-        holder.showUrl("http://example.com")
+        holder.showUrl(model.itemAt(position))
     }
 
-    override fun getItemCount(): Int {
-        return 3
-    }
+    override fun getItemCount() = model.countItems()
 }
---
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/model/StashListModel.kt
@@ -0,0 +1,17 @@
+package com.github.polydome.popstash.app.model
+
+import javax.inject.Inject
+
+class StashListModel @Inject constructor() {
+    private val items = listOf(
+            "http://google.com",
+            "http://example.com",
+            "http://medium.com"
+        )
+
+    fun itemAt(position: Int): String
+        = items[position]
+
+    fun countItems()
+        = items.count()
+}
