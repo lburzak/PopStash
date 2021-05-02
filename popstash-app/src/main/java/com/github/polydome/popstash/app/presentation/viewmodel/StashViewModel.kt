8bbec96aaaaf04bee28dc2b3a91ff3acb593321c
polydome <polydome@outlook.com>
2021-05-02 19:27:09 +0200

    Cleanup `StashViewModel`

--- popstash-app/src/main/java/com/github/polydome/popstash/app/presentation/viewmodel/StashViewModel.kt
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/presentation/viewmodel/StashViewModel.kt
@@ -6,27 +6,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.polydome.popstash.app.presentation.service.Clipboard
 import com.github.polydome.popstash.app.presentation.service.PatternMatcher
-import com.github.polydome.popstash.app.util.Millis
-import com.github.polydome.popstash.app.util.intervalFlow
 import com.github.polydome.popstash.domain.usecase.ListStashedUrls
 import com.github.polydome.popstash.domain.usecase.SaveResource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class StashViewModel @Inject constructor(
         private val saveResource: SaveResource,
-        private val listStashedUrls: ListStashedUrls,
         private val clipboard: Clipboard,
         private val patternMatcher: PatternMatcher,
+        listStashedUrls: ListStashedUrls,
 ) : ViewModel() {
-
-    private val clipboardContents =
-            intervalFlow(Millis.ofSeconds(5), clipboard::getText)
     private val _isUrlInClipboard = MutableLiveData<Boolean>()
 
     val isUrlInClipboard: LiveData<Boolean> = _isUrlInClipboard
@@ -47,13 +40,5 @@ class StashViewModel @Inject constructor(
 
         if (patternMatcher.matchUrl(content))
             _isUrlInClipboard.postValue(true)
-    }
-
-    fun monitorClipboard() {
-        viewModelScope.launch {
-            clipboardContents
-                    .map(patternMatcher::matchUrl)
-                    .collect(_isUrlInClipboard::postValue)
-        }
     }
 }
