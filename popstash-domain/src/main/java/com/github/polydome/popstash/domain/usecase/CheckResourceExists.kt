32a1b2fb9558917a9bfc58d980518040a72f810c
polydome <polydome@outlook.com>
2021-05-03 18:06:02 +0200

    Implement 'check resource exists' use case

---
+++ popstash-domain/src/main/java/com/github/polydome/popstash/domain/usecase/CheckResourceExists.kt
@@ -0,0 +1,8 @@
+package com.github.polydome.popstash.domain.usecase
+
+import com.github.polydome.popstash.domain.repository.ResourceRepository
+
+class CheckResourceExists(private val resourceRepository: ResourceRepository) {
+    suspend fun execute(url: String): Boolean =
+            resourceRepository.existsResource(url)
+}
---
+++ popstash-domain/src/test/kotlin/com/github/polydome/popstash/domain/usecase/CheckResourceExistsTest.kt
@@ -0,0 +1,49 @@
+package com.github.polydome.popstash.domain.usecase
+
+import app.cash.turbine.test
+import com.github.polydome.popstash.domain.repository.ResourceRepository
+import com.github.polydome.popstash.domain.usecase.test.hotFlowOf
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+internal class CheckResourceExistsTest {
+    companion object {
+        val URL = "http://example.com"
+    }
+
+    private val resourceRepository = mockk<ResourceRepository>()
+    private val sut: CheckResourceExists = CheckResourceExists(resourceRepository)
+
+    @Nested
+    inner class `given resource not exists` {
+        init {
+            coEvery { resourceRepository.existsResource(URL) } returns false
+        }
+
+        @Test
+        internal fun `when execute then emits false`() = runBlocking {
+            val urlExists = sut.execute(URL)
+            assertThat(urlExists).isFalse()
+        }
+    }
+
+    @Nested
+    inner class `given resource exists` {
+        init {
+            coEvery { resourceRepository.existsResource(URL) } returns true
+        }
+
+        @Test
+        internal fun `when execute then emits true`() = runBlocking {
+            val urlExists = sut.execute(URL)
+            assertThat(urlExists).isTrue()
+        }
+    }
+}
