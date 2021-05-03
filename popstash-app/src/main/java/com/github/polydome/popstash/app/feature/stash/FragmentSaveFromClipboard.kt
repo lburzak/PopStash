8faa082ce4190082a0991d8c4d78d396fea34798
polydome <polydome@outlook.com>
2021-05-03 14:15:32 +0200

    Add "save from clipboard" dialog to `MainActivity` layout

---
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/feature/stash/FragmentSaveFromClipboard.kt
@@ -0,0 +1,6 @@
+package com.github.polydome.popstash.app.feature.stash
+
+import androidx.fragment.app.Fragment
+import com.github.polydome.popstash.app.R
+
+class FragmentSaveFromClipboard : Fragment(R.layout.dialog_save_from_clipboard)
