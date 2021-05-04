7b7ff0bcf139c937e50455738d84d0e4c1d0800c
polydome <polydome@outlook.com>
2021-05-04 14:37:49 +0200

    Rewrite "save from clipboard" showing logic with clipboard flow

--- popstash-app/src/main/java/com/github/polydome/popstash/app/presentation/viewmodel/SaveFromClipboardViewModel.kt
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/presentation/viewmodel/SaveFromClipboardViewModel.kt
@@ -1,14 +1,13 @@
 package com.github.polydome.popstash.app.presentation.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.github.polydome.popstash.app.presentation.service.Clipboard
 import com.github.polydome.popstash.app.presentation.service.PatternMatcher
 import com.github.polydome.popstash.domain.usecase.CheckResourceInStash
 import com.github.polydome.popstash.domain.usecase.SaveResource
+import com.github.polydome.popstash.domain.usecase.WatchResourceExists
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -16,14 +15,30 @@ import javax.inject.Inject
 class SaveFromClipboardViewModel @Inject constructor(
         private val saveResource: SaveResource,
         private val checkResourceInStash: CheckResourceInStash,
+        private val watchResourceExists: WatchResourceExists,
         private val clipboard: Clipboard,
         private val patternMatcher: PatternMatcher
 ) : ViewModel() {
     private val _shouldDisplayDialog = MutableLiveData<Boolean>()
     private val _urlInClipboard = MutableLiveData<String>()
 
-    val shouldDisplayDialog: LiveData<Boolean> = _shouldDisplayDialog
-    val urlInClipboard: LiveData<String> = _urlInClipboard
+    private val isUrlInClipboard = clipboard.contents()
+            .map { patternMatcher.matchUrl(it) }
+
+    private val urlsFromClipboard = clipboard.contents()
+            .filter { patternMatcher.matchUrl(it) }
+
+    private val lastUrlExists = urlsFromClipboard
+            .flatMapLatest { url -> watchResourceExists.execute(url) }
+
+    val shouldDisplayDialog: LiveData<Boolean> =
+            isUrlInClipboard
+                    .combine(lastUrlExists) { isUrl, alreadyExists -> isUrl && !alreadyExists }
+                    .asLiveData()
+
+    val urlInClipboard: LiveData<String> =
+            urlsFromClipboard
+                    .asLiveData()
 
     fun saveUrlFromClipboard() {
         val url = clipboard.getText() ?: return
