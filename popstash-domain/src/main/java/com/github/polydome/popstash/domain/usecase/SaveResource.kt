0cec29de6e6fff1263c5a1f9d1936006f8002da9
polydome <polydome@outlook.com>
2021-05-07 18:25:34 +0200

    Fix save resource use case accepting existing urls

--- popstash-domain/src/main/java/com/github/polydome/popstash/domain/usecase/SaveResource.kt
+++ popstash-domain/src/main/java/com/github/polydome/popstash/domain/usecase/SaveResource.kt
@@ -5,9 +5,24 @@ import com.github.polydome.popstash.domain.repository.ResourceRepository
 import javax.inject.Inject
 
 class SaveResource @Inject constructor(private val resourceRepository: ResourceRepository) {
-    suspend fun execute(url: String) {
+    suspend fun execute(url: String): Result {
+        val resourceExists = resourceRepository.existsResourceByUrl(url)
+
+        if (resourceExists)
+            return Result.Failure(Error.RESOURCE_ALREADY_EXISTS)
+
         val resource = Resource(url = url)
 
         resourceRepository.insertOne(resource)
+        return Result.Success
+    }
+
+    sealed class Result {
+        object Success: Result()
+        data class Failure(val error: Error): Result()
+    }
+
+    enum class Error {
+        RESOURCE_ALREADY_EXISTS
     }
 }
--- popstash-domain/src/test/kotlin/com/github/polydome/popstash/domain/usecase/SaveResourceTest.kt
+++ popstash-domain/src/test/kotlin/com/github/polydome/popstash/domain/usecase/SaveResourceTest.kt
@@ -2,6 +2,11 @@ package com.github.polydome.popstash.domain.usecase
 
 import com.github.polydome.popstash.domain.model.Resource
 import com.github.polydome.popstash.domain.repository.ResourceRepository
+import com.github.polydome.popstash.domain.usecase.SaveResource.Error
+import com.github.polydome.popstash.domain.usecase.SaveResource.Result.Failure
+import com.github.polydome.popstash.domain.usecase.SaveResource.Result.Success
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
@@ -11,24 +16,47 @@ import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 internal class SaveResourceTest {
-    val resourceRepository = mockk<ResourceRepository>(relaxed = true)
+    val resourceRepository = mockk<ResourceRepository>(relaxUnitFun = true) {
+        coEvery { existsResourceByUrl(EXISTING_RESOURCE_URL) } returns true
+        coEvery { existsResourceByUrl(VALID_URL) } returns false
+    }
+
     val sut = SaveResource(resourceRepository)
 
     @Nested
     inner class `given valid url` {
-        private val url = "http://example.com/article-slug"
-
         @Test
         internal fun `when execute then save resource`() {
             runBlocking {
-                sut.execute(url)
+                sut.execute(VALID_URL)
 
-                val expectedResource = Resource(
-                        url = url
-                )
-
-                coVerify(exactly = 1) { resourceRepository.insertOne(expectedResource) }
+                coVerify(exactly = 1) { resourceRepository.insertOne(EXPECTED_RESOURCE) }
             }
         }
+
+        @Test
+        internal fun `when execute then return success`(): Unit = runBlocking {
+            val result = sut.execute(VALID_URL)
+
+            assertThat(result).isInstanceOf(Success::class.java)
+        }
+    }
+
+    @Nested
+    inner class `given existing resource url` {
+        @Test
+        internal fun `when execute then return failure`(): Unit = runBlocking {
+            val result = sut.execute(EXISTING_RESOURCE_URL)
+
+            assertThat(result).isInstanceOf(Failure::class.java)
+            assertThat((result as Failure).error).isEqualTo(Error.RESOURCE_ALREADY_EXISTS)
+        }
+    }
+
+    companion object {
+        const val VALID_URL = "http://example.com/article-slug"
+        val EXPECTED_RESOURCE = Resource(url = VALID_URL)
+
+        const val EXISTING_RESOURCE_URL = "http://example.com/mberlioz/final-destination"
     }
 }
