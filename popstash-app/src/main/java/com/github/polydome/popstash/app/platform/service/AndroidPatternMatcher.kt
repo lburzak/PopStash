c58b0e48a3c17b09144661031cfe037ea81c0d79
polydome <polydome@outlook.com>
2021-05-07 18:35:45 +0200

    Implement `URLValidator`

--- popstash-app/src/main/java/com/github/polydome/popstash/app/platform/service/AndroidPatternMatcher.kt
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/platform/service/AndroidPatternMatcher.kt
@@ -1,9 +1,13 @@
 package com.github.polydome.popstash.app.platform.service
 
 import com.github.polydome.popstash.app.presentation.service.PatternMatcher
+import com.github.polydome.popstash.domain.service.URLValidator
 import javax.inject.Inject
 
-class AndroidPatternMatcher @Inject constructor() : PatternMatcher {
+class AndroidPatternMatcher @Inject constructor() : PatternMatcher, URLValidator {
     override fun matchUrl(text: String): Boolean =
             android.util.Patterns.WEB_URL.matcher(text).matches()
+
+    override fun validateUrl(url: String): Boolean =
+            matchUrl(url)
 }
