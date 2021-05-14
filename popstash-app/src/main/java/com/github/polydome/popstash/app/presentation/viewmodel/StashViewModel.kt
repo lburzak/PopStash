99563b22d30f8a488b3ad1cd8b9aa8113463c9fc
polydome <polydome@outlook.com>
2021-05-14 19:31:51 +0200

    Make "stash empty" tip visible only when stash is empty

--- popstash-app/src/main/java/com/github/polydome/popstash/app/presentation/viewmodel/StashViewModel.kt
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/presentation/viewmodel/StashViewModel.kt
@@ -1,10 +1,17 @@
 package com.github.polydome.popstash.app.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.github.polydome.popstash.domain.usecase.ListStashedUrls
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class StashViewModel @Inject constructor(listStashedUrls: ListStashedUrls) : ViewModel() {
     val urls: Flow<List<String>> = listStashedUrls.execute()
+    val stashEmpty = urls
+            .map { urls -> urls.count() }
+            .map { urlsCount -> urlsCount == 0 }
+            .asLiveData(viewModelScope.coroutineContext)
 }
