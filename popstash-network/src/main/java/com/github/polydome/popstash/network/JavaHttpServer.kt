dc27cc030c653ef12b9fdc424c7f6aa5b3b569a8
polydome <polydome@outlook.com>
2021-05-14 17:54:18 +0200

    Fix unhandled exception when fetching from non-existing resource

--- popstash-network/src/main/java/com/github/polydome/popstash/network/JavaHttpServer.kt
+++ popstash-network/src/main/java/com/github/polydome/popstash/network/JavaHttpServer.kt
@@ -1,14 +1,21 @@
 package com.github.polydome.popstash.network
 
 import com.github.polydome.popstash.parser.service.HttpServer
+import com.github.polydome.popstash.parser.service.HttpServer.Error
 import com.github.polydome.popstash.parser.service.HttpServer.Result
+import java.io.FileNotFoundException
 import java.net.URL
 import javax.inject.Inject
 
 class JavaHttpServer @Inject constructor() : HttpServer {
     override fun fetchHtml(url: String): Result {
         val connection = URL(url).openConnection()
-        val inputStream = connection.getInputStream()
+
+        val inputStream = try {
+            connection.getInputStream()
+        } catch (e: FileNotFoundException) {
+            return Result.Failure(Error.NO_SUCH_RESOURCE)
+        }
 
         val html = inputStream.reader().useLines {
             it.joinToString()
