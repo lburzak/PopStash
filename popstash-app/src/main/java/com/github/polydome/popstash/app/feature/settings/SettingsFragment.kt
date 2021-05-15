d90b808b6c85cc644e69ce1c7112a91de3ab046b
polydome <polydome@outlook.com>
2021-05-15 18:48:59 +0200

    Implement theme change handler

--- popstash-app/src/main/java/com/github/polydome/popstash/app/feature/settings/SettingsFragment.kt
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/feature/settings/SettingsFragment.kt
@@ -1,11 +1,38 @@
 package com.github.polydome.popstash.app.feature.settings
 
 import android.os.Bundle
+import androidx.annotation.StyleRes
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.github.polydome.popstash.app.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        val themePreference = findPreference<ListPreference>("theme")
+
+        themePreference?.setOnPreferenceChangeListener { _, newValue ->
+            val theme = themePreference.parseThemeValue(newValue)
+            onThemeChange(theme)
+
+            true
+        }
+    }
+
+    private fun onThemeChange(theme: Theme) {
+        val activity = requireActivity()
+        activity.setTheme(theme.resId)
+    }
+
+    private fun ListPreference.parseThemeValue(value: Any): Theme {
+        val valueIndex = findIndexOfValue(value as String?)
+        return Theme.values()[valueIndex]
+    }
+
+    private enum class Theme(@StyleRes val resId: Int) {
+        LIGHT(R.style.AppTheme),
+        DARK(R.style.AppTheme_Dark)
     }
 }
