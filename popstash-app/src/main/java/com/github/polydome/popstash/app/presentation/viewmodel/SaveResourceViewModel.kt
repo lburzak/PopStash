b727830082f0fe1664abd87d825fbbdb7da7b1d8
polydome <polydome@outlook.com>
2021-05-25 09:36:05 +0200

    Implement `SaveResourceViewModel`

--- popstash-app/src/main/java/com/github/polydome/popstash/app/presentation/viewmodel/SaveResourceViewModel.kt
+++ popstash-app/src/main/java/com/github/polydome/popstash/app/presentation/viewmodel/SaveResourceViewModel.kt
@@ -1,13 +1,18 @@
 package com.github.polydome.popstash.app.presentation.viewmodel
 
+import com.github.polydome.popstash.domain.usecase.SaveResource
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
-class SaveResourceViewModel @Inject constructor() {
+class SaveResourceViewModel @Inject constructor(private val saveResource: SaveResource) {
     val url = MutableSharedFlow<String>()
     val outcome = url
-            .map { Outcome.SUCCESS }
+            .map { url -> saveResource.execute(url) }
+            .map { result -> when (result) {
+                is SaveResource.Result.Success -> Outcome.SUCCESS
+                is SaveResource.Result.Failure -> Outcome.FAILURE
+            } }
 
     enum class Outcome {
         SUCCESS,
